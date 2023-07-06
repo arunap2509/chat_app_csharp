2ae75dee17e46e8c975b5b59bf9fe1022c2daadc
Arun AP <arunap@192.168.0.103>
2023-07-06 21:27:15 +0530

    Added validation

---
+++ Validators/RegisterRequestValidator.cs
@@ -0,0 +1,29 @@
+using ChatApp.Data;
+using ChatApp.Dto;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatApp.Validators;
+
+public class RegisterRequestValidator : AbstractValidator<RegisterRequest>
+{
+    public RegisterRequestValidator(AppDbContext dbContext)
+    {
+        RuleFor(x => x.Email).NotEmpty().EmailAddress().WithMessage("Invalid email address");
+        RuleFor(x => x.Password).NotEmpty().Must(x => x.Length > 8).WithMessage("{PropertyName} should be minimum of 8 characters");
+
+        RuleFor(x => x.Email).MustAsync(async (email, cancallationToken) => await ValidateEmailAlreadyExist(email, dbContext)).WithMessage("Email already exists");
+
+        RuleFor(x => x.UserName).MustAsync(async (userName, cancallationToken) => await ValidateUserNameAlreadyExist(userName, dbContext)).WithMessage("Username already exists");
+    }
+
+    private async Task<bool> ValidateEmailAlreadyExist(string email, AppDbContext dbContext)
+    {
+        return !await dbContext.Users.AnyAsync(x => x.Email == email);
+    }
+
+    private async Task<bool> ValidateUserNameAlreadyExist(string userName, AppDbContext dbContext)
+    {
+        return !await dbContext.Users.AnyAsync(x => x.UserName == userName);
+    }
+}
